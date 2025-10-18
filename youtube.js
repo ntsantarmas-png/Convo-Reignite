aaff339ab3c22277f9cd75ab075dae538809020e
ntsantarmas-png <ntsantarmas@gmail.com>
2025-10-18 19:15:20 +0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -122,13 +122,15 @@ async function showVideo(videoId) {
   youtubePanel.classList.remove("hidden");
 
   youtubeContent.innerHTML = `
-    <iframe width="100%" height="200"
-      src="https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen>
-    </iframe>
-  `;
+  <iframe width="100%" height="200"
+    src="https://www.youtube.com/embed/${videoId}?autoplay=1&mute=0&rel=0"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allow="autoplay; encrypted-media"
+    allowfullscreen>
+  </iframe>
+`;
+
   console.log("🎵 YouTube video loaded:", videoId);
 
   try {
